8da3d7f55d38692a501552a20472b2bad97266d8
vladgemtyp <vlad.gem.typ@gmail.com>
2018-02-12 15:49:02 +0200

    config has been changed

--- config/module.config.php
+++ config/module.config.php
@@ -1,20 +1,25 @@
 <?php
+
 namespace Stagem\ZfcAction;
 
 return [
     'routes' => [
         [
-             'name' => 'home',
-             'path' => '/',
-             'middleware' => \App\Action\HomePageAction::class,
-             'allowed_methods' => ['GET'],
+            'name' => 'home',
+            'path' => '/',
+            'middleware' => [\Stagem\Layout\Action\HomeAction::class, Page\RendererMiddleware::class],
+            'allowed_methods' => ['GET'],
+            'options' => [ // this don't work automatically, you need check this by hand
+                'resource' => 'layout',
+                'action' => 'home',
+            ]
         ],
 
         // for detail @see https://github.com/nikic/FastRoute/issues/103
         [
             'name' => 'default/action',
             'path' => '/{resource:[a-z-]{3,}}[/[{action:[a-z-]{3,}}[/[{id:\d+}[/{more:.*}]]]]]',
-            'middleware' => Page\ConnectivePage::class,
+            'middleware' => [Page\ConnectivePage::class, Page\RendererMiddleware::class],
             'options' => [ // this don't work automatically, you need check this by hand
                 'resource' => 'index',
                 'action' => 'index',
