647cb9d543bd077146893e63e30c10362582c66a
popovsergiy <popow.sergiy@gmail.com>
2018-02-25 20:24:14 +0200

    Route has been improved

--- config/module.config.php
+++ config/module.config.php
@@ -18,7 +18,7 @@ return [
         // for detail @see https://github.com/nikic/FastRoute/issues/103
         [
             'name' => 'default',
-            'path' => '/{resource:[a-z-]{3,}}[/[{action:[a-z-]{3,}}[/[{id:\d+}[/{more:.*}]]]]]',
+            'path' => '/{resource:[a-z-]{3,}}[/[{action:[a-z-]{3,}}[/{id:\d+}[/{more:.*}]]]]',
             'middleware' => [Page\ConnectivePage::class, Page\RendererMiddleware::class],
             'options' => [ // this don't work automatically, you need check this by hand
                 'resource' => 'index',
