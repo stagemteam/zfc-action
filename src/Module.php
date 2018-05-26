c875f45d78da748cad357f5d39b78a9367d40fd4
popovsergiy <popow.sergiy@gmail.com>
2018-05-26 14:38:48 +0300

    Module.php has been added

---
+++ src/Module.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2018 Stagem Team
+ * This source file is subject to The MIT License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/MIT
+ *
+ * @category Stagem
+ * @package Stagem_ZfcRoute
+ * @author Serhii Popov <popow.serhii@gmail.com>
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Stagem\ZfcAction;
+
+class Module
+{
+    public function getConfig()
+    {
+        $config = include __DIR__ . '/../config/module.config.php';
+        $config['service_manager'] = $config['dependencies'];
+        unset($config['dependencies']);
+        unset($config['routes']);
+
+        return $config;
+    }
+}
